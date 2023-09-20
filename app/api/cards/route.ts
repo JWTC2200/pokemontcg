8e124273e0a2807b928f20520969dedba22b9538
JWTC2200 <113539489+JWTC2200@users.noreply.github.com>
2023-09-20 17:45:52 +0100

    remove standard limit from query string

--- app/api/cards/route.ts
+++ app/api/cards/route.ts
@@ -8,7 +8,7 @@ export const GET = async (request: Request) => {
 
     try {
         // const res = await PokemonTCG.Card.where({ q: `${name} ${mark} legalities.standard:legal`})
-        const res = await PokemonTCG.findCardsByQueries({ q: `${queryString} legalities.standard:legal` })
+        const res = await PokemonTCG.findCardsByQueries({ q: `${queryString}` })
         if(!res) return new Response("Card not found", {status: 404})
         return new Response(JSON.stringify(res), {status: 201})
     } catch (error) {
